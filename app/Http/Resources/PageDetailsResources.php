7ea8d1d223fd33c195ff53c2692b1bd0baf1fb47
mohamednasser-dev <72683413+mohamednasser-dev@users.noreply.github.com>
2022-11-19 15:14:11 +0200

    page details resources

---
+++ app/Http/Resources/PageDetailsResources.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PageDetailsResources extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'image' => $this->image,
+            'title' => $this->title,
+            'body' => $this->body,
+            'icon' => $this->icon,
+        ];
+    }
+}
