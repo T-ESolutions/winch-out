0ed73ebec6d2ee8c652e3be13ebd18cb37bcc43a
MostafaElebzary <mostafaelebzary@gmail.com>
2022-11-19 15:30:54 +0200

    Model Year Table

---
+++ app/Models/ModellYear.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ModellYear extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+//    modell_id
+//year
+
+    public function modell()
+    {
+        return $this->belongsTo(Modell::class, 'modell_id');
+    }
+
+}
---
+++ database/migrations/2022_11_19_132901_create_modell_years_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('modell_years', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('modell_id')->references('id')->on('modells')->onDelete('cascade');
+            $table->string('year');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('modell_years');
+    }
+};
